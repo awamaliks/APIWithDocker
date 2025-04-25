1ee8b4472def6207ee333c9bf4b8ae36d5b922e9
Asim Javed <asim.javed@dataqhealth.com>
2025-04-25 20:26:00 +0500

    swagger build issue fixation

--- ProductsServices/Program.cs
+++ ProductsServices/Program.cs
@@ -1,30 +1,22 @@
-var builder = WebApplication.CreateBuilder(args);
+using ProductsServices;
 
-// Add services to the container.
-
-builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-
-builder.WebHost.ConfigureKestrel(options =>
+public class Program
 {
-    options.ListenAnyIP(80); // Use HTTP only
-});
+    public static void Main(string[] args)
+    {
+        CreateHostBuilder(args).Build().Run();
+    }
 
-var app = builder.Build();
+    public static IHostBuilder CreateHostBuilder(string[] args) =>
+        Host.CreateDefaultBuilder(args)
+            .ConfigureWebHostDefaults(webBuilder =>
+            {
+                webBuilder.UseStartup<Startup>();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
+                // Your custom Kestrel config
+                webBuilder.ConfigureKestrel(options =>
+                {
+                    options.ListenAnyIP(80);
+                });
+            });
 }
-
-app.UseHttpsRedirection();
-
-app.UseAuthorization();
-
-app.MapControllers();
-
-app.Run();
